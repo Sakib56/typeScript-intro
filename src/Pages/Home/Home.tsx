e5ac5978c54b8e507945c28b2704471566715ba1
Sakib <sakib.hstu.bd@gmail.com>
2023-09-14 15:01:19 +0600

    some code updated

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,19 +4,20 @@ import bikesData from "../../Data/Bikes";
 
 
 export type BikesType = {
-  id?: number;
-  title: string;
-  price: number;
-  image: string;
+    id?: number;
+    title: string;
+    price: number;
+    image: string;
 };
+
 const Home = () => {
     return (
         <div className="py-10">
             <h1 className="text-center font-bold md:text-2xl">Welcome to Bikes Bd shop</h1>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-            {bikesData.map((book) => (
-          <Card key={book.id} {...book} />
-        ))}
+                {bikesData.map((bike) => (
+                    <Card key={bike.id} {...bike} />
+                ))}
             </div>
         </div>
     );
