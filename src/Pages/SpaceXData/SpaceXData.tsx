e5cbe77d56310303a3ef42604f32a999857e6ee4
Sakib <sakib.hstu.bd@gmail.com>
2023-10-03 14:06:38 +0600

    spacex data code fixed

--- src/Pages/SpaceXData/SpaceXData.tsx
+++ src/Pages/SpaceXData/SpaceXData.tsx
@@ -1,18 +1,22 @@
 import { useQuery } from "@apollo/client";
 import { GET_SPACEX_DATA } from "../../graphql/queries";
+import { History } from "../../graphql/__generated__/graphql";
 
 const SpaceXData = () => {
     const { loading, error, data } = useQuery(GET_SPACEX_DATA);
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
+    const histories = data?.histories
 
     return (
         <div>
             {
-                data?.histories?.map((history: any)=>(
-                    <div key={history.id}>
-                        <p>{history.title}</p>
-                        <p>{history.details}</p>
+                histories?.map((history:History | null)=>(
+                    <div key={history?.id}>
+                        <p>{history?.title}</p>
+                        <p>{history?.details}</p>
+                        <p>{history?.__typename}</p>
+                        <p>{history?.event_date_utc}</p>
                     </div>
                 ))
             }
